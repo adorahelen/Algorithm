dd1420f92461662300a315aa0fe1243976aec6e0
adorahelen <adorahelenmin@gmail.com>
2024-03-14 21:16:44 +0900

    24_03_14 DCU_lib

---
+++ src/DataStruct/ch01_SumFor.java
@@ -0,0 +1,23 @@
+package DataStruct;
+
+import java.util.Scanner;
+public class ch01_SumFor {
+
+    public static void main(String[] args) {
+        Scanner DCU = new Scanner(System.in);
+
+        System.out.println(" Make the Sum from 1, to N .");
+        System.out.print("Input The 'N' value ...: ");
+
+        int n = DCU.nextInt();
+
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++)
+            sum += i;
+
+
+        System.out.println("from 1,  to " + n + "... Sum is " + sum);
+    }
+}
---
+++ src/DataStruct/ch01_SumOnlyP.java
@@ -0,0 +1,23 @@
+package DataStruct;
+import java.util.Scanner;
+public class ch01_SumOnlyP {
+
+    public static void main(String[] args) {
+        Scanner DCU = new Scanner(System.in);
+
+        int n;
+
+        System.out.println("Sum of the 1 to N");
+
+        do {
+            System.out.print("N value : ");
+            n = DCU.nextInt();
+        } while (n <= 0); // 0보다 클때까지 무한 반복
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+            sum += i;
+
+        System.out.println(" 1 to N sum is  " + sum + "<<<");
+    }
+}
