a2df32d7a807115d751db7997d76e1f010b9a576
adorahelen <adorahelenmin@gmail.com>
2024-04-14 13:55:25 +0900

    24_04_14 DCU_centLib

---
+++ src/DataStruct/CH04_StackQue/QueRingBuffer.java
@@ -0,0 +1,37 @@
+package DataStruct.CH04_StackQue;
+
+public class QueRingBuffer {
+    // 생성할 때 용량을 결정하는 고정 길이 큐이다.
+    // int형 데이터를 저장한다.
+
+    private int[] que; // 큐용 배열
+    private int capacity; // 큐의 용량
+    private int front; // 맨 앞 요소 커서
+    private int rear; // 맨 뒤 요소 커서
+    private int num; // 현재 데이터 개수
+
+    // 실행 시 예외 큐가 비어 있는 경우
+    public class EmptyIntQueueException extends RuntimeException {
+        public EmptyIntQueueException() { }
+    }
+
+    // 실행 시 예외 큐가 가득 찬 경우
+    public class OverflowQueueException extends RuntimeException {
+        public OverflowQueueException() { }
+    }
+
+    // 생성자
+    public QueRingBuffer(int maxlen) {
+        num = front = rear = 0;
+        // 생성시 큐는 비어있기 때문에 값을 모두 0으로 한다.
+        capacity = maxlen;
+        try {
+            que = new int [capacity]; // 큐 본체용 배열 생성
+        } catch (OutOfMemoryError e) {
+            // 생성할 수 없음
+        }
+    }
+
+
+
+}
