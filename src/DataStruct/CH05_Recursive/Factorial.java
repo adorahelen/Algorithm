6fde7ece455351493de86232b5deef0920652c51
adorahelen <adorahelenmin@gmail.com>
2024-04-22 22:43:15 +0900

    24_04_22 Take Responsibility

---
+++ src/DataStruct/CH05_Recursive/Factorial.java
@@ -0,0 +1,22 @@
+package DataStruct.CH05_Recursive;
+
+import java.util.Scanner;
+
+public class Factorial {
+    static int factorial (int n) {
+        if (n > 0)
+            return n * factorial(n - 1);
+        else
+            return  1;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner DCU = new Scanner(System.in);
+
+        System.out.print(" 정수를 입력하세요 : ");
+        int x = DCU.nextInt();
+
+        System.out.println(x + " 의 팩토리얼은 " + factorial(x) + "입니다.");
+    }
+}
